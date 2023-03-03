916a8c4ab02366bcf69e143468e9cef3915fe4a5
Sushma Suresh <ssureshbabu@umass.edu>
2023-03-02 19:27:52 -0500

    added

--- com/test/Library.java
+++ com/test/Library.java
@@ -3,5 +3,7 @@ package com.test;
 public class Library {
     int lib_id;
     String book_name;
-
+static {
+    System.out.println("starting this coding");
+}
 }
---
+++ com/test/test.java
@@ -0,0 +1,9 @@
+package com.test;
+class test{
+    public static void  main(String[] args) {
+        Student s1= new Student();
+        Library L1=new Library();
+    }
+
+
+}
