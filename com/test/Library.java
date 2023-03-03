da63d29e0b0a7dc0643dadb03ed63dabc6229def
Sushma Suresh <ssureshbabu@umass.edu>
2023-03-02 19:31:28 -0500

    added

--- com/test/Library.java
+++ com/test/Library.java
@@ -3,7 +3,12 @@ package com.test;
 public class Library {
     int lib_id;
     String book_name;
-static {
-    System.out.println("starting this coding");
-}
+
+    static {
+        System.out.println("starting this coding");
+    }
+
+    public Library() {
+
+    }
 }
