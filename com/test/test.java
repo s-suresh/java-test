4ba1a883f2dff93cd6f5452cdaa0ef723a0298d3
Sushma Suresh <ssureshbabu@umass.edu>
2023-03-14 22:49:06 -0400

    added

--- com/test/test.java
+++ com/test/test.java
@@ -1,9 +1,14 @@
 package com.test;
+
+import java.utils.Hashset;
 class test{
     public static void  main(String[] args) {
         Student s1= new Student();
         Library L1=new Library();
     }
+    Hashset set = new Hashset();
+    set.add(56)
+    set.add(78)
 
 
 }
